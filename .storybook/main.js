36a3609dd12854cf754ea070dd453a458e6a29de
刘圣伟 <1064084779@qq.com>
2022-02-15 17:49:43 +0800

    finish sencond steps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+    stories: [
+        // "../src/**/*.stories.mdx",
+        // "../src/**/*.stories.@(js|jsx|ts|tsx)",
+        '../src/components/**/*.stories.js',
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+    ],
 };
