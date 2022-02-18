4b3e467eb81b8451db4e6b09e9fc28e6988acc89
刘圣伟 <1064084779@qq.com>
2022-02-18 14:58:33 +0800

    change task background to red

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -5,7 +5,7 @@
       <span class="checkbox-custom" @click="archiveTask" />
     </label>
     <div class="title">
-      <input type="text" :value="task.title" readonly placeholder="Input title" />
+      <input type="text" :value="task.title" readonly placeholder="Input title" style="background: red;" />
     </div>
     <div class="actions">
       <a v-if="!isChecked" @click="pinTask">
